662309b037735cc33a9427f52afda7c17a23bc58
Herbert Campos <wohoforever@yahoo.com>
2019-06-30 00:12:28 -0700

    set up mysql connection and have set up to show the entire table via console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,30 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "Odalis1998!",
+    database: "bamazon"
+  });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    showItems();
+});
+
+function showItems() {
+    console.log("Selecting all items...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        var data = res;
+        askUser(data);
+});  
+}
